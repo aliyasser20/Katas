17e398a7b1854d3d19d3af0815b0e2d89e42f501
ALi <aliyasser20@hotmail.com>
2019-12-09 02:30:51 +0000

    completed kata 16

---
+++ Kata_16.js
@@ -0,0 +1,61 @@
+const blocksAway = function(directions) {
+  let calulcatedBlocksAway = {
+    east: 0,
+    north: 0
+  };
+  let numberOfRepetitions = 0;
+
+  for (let i = 0; i < directions.length; i += 2) {
+    if (directions[i] != directions[i - 2]) {
+      numberOfRepetitions = 0;
+    } else {
+      numberOfRepetitions += 1;
+    }
+    switch (directions[i]) {
+      case "right":
+        if (numberOfRepetitions === 0) {
+          calulcatedBlocksAway.east += Number(directions[i + 1]);
+        } else {
+          switch (numberOfRepetitions) {
+            case 1:
+              calulcatedBlocksAway.north = calulcatedBlocksAway.north - Number(directions[i + 1]);
+              break;
+            case 2:
+                calulcatedBlocksAway.east = calulcatedBlocksAway.east - Number(directions[i + 1]);
+              break;
+            case 3:
+              calulcatedBlocksAway.north += Number(directions[i + 1]);
+              numberOfRepetitions = 0;
+              break;
+          }
+        }
+        break;
+      case "left":
+        if (numberOfRepetitions === 0) {
+          calulcatedBlocksAway.north += Number(directions[i + 1]);
+        } else {
+          switch (numberOfRepetitions) {
+            case 1:
+              calulcatedBlocksAway.east = calulcatedBlocksAway.east - Number(directions[i + 1]);
+              break;
+            case 2:
+              calulcatedBlocksAway.north += Number(directions[i + 1]);
+              break;
+            case 3:
+              calulcatedBlocksAway.east += Number(directions[i + 1]);
+              break;
+            case 4:
+              calulcatedBlocksAway.north = calulcatedBlocksAway.north - Number(directions[i + 1]);
+              numberOfRepetitions = 0;
+              break;
+          }
+        }        
+        break;
+    }
+  }
+  return calulcatedBlocksAway;
+};
+
+console.log(blocksAway(["right", 2, "left", 3, "left", 1]));
+console.log(blocksAway(["left", 1, "right", 1, "left", 1, "right", 1, "left", 1, "right", 1]));
+console.log(blocksAway(["left", 3, "right", 1, "right", 3, "right", 1]));
